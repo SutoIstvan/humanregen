efa01290bd81f488713582eb496f19c3942edd56
SutoIstvan <isutoi@gmail.com>
2024-11-12 11:08:35 +0200

    update

---
+++ resources/lang/hu/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'A jelszó visszaállítva!',
+    'sent' => 'Elküldtük a jelszó helyreállítási linket!',
+    'throttled' => 'Kérjük várjon, mielőtt újra próbálkozik.',
+    'token' => 'A jelszó helyreállítási token érvénytelen.',
+    'user' => 'Nem található felhasználó a megadott email címmel.',
+];
