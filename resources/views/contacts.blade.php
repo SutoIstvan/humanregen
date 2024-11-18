6e6e81978b20ceb699e5327a0e099e74ef310f09
SutoIstvan <isutoi@gmail.com>
2024-11-18 10:13:37 +0200

    update

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -69,36 +69,90 @@
 
   </section><!-- /Hero Section -->
 
+  <div class="px-4 pt-5 my-5 text-center ">
+    <h2 class="alex-brush-regular" style="color: #008288; font-size: 38px;">Kapcsolat</h2>
+    <div class="col-lg-10 mx-auto">
+      <p class="lead mb-4">Az alábbi elérhetőségeken léphet velünk kapcsolatba</p>
+    </div>  
+  </div>
+
   {{-- <div class="container d-flex justify-content-center">
+    
     <div class="col-10">
+      
       <div class="row">
-        <div class="col-lg-4 text-center">
-          <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
-          <h2 class="fw-normal">Heading</h2>
-          <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-          <p><a class="btn btn-secondary" href="#">View details »</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4 text-center">
-          <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
-          <h2 class="fw-normal">Heading</h2>
-          <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-          <p><a class="btn btn-secondary" href="#">View details »</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4 text-center">
-          <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
-          <h2 class="fw-normal">Heading</h2>
-          <p>And lastly this, the third column of representative placeholder content.</p>
-          <p><a class="btn btn-secondary" href="#">View details »</a></p>
-        </div><!-- /.col-lg-4 -->
+        
+        <div class="col-lg-4 text-center card mb-5 rounded-3 shadow-sm">
+          <div class="card-body">
+            <i class="fa-solid fa-envelope fa-2xl" style="color: #008288"></i>
+            <h2 class="alex-brush-regular mt-4" style="color: #008288; font-size: 38px;">Email:</h2>
+            <p>info@humanregen.hu</p>
+          </div>
+        </div>
+        
+        <div class="col-lg-4 text-center card mb-5 rounded-3 shadow-sm">
+          <div class="card-body">
+            <i class="fa-solid fa-phone fa-2xl" style="color: #008288"></i>
+            <h2 class="alex-brush-regular mt-4" style="color: #008288; font-size: 38px;">Telefon:</h2>
+            <p>+36 30 39 677 67</p>
+          </div>
+        </div>
+        
+        <div class="col-lg-4 text-center card mb-5 rounded-3 shadow-sm">
+          <div class="card-body">
+            <i class="fa-solid fa-location-dot fa-2xl" style="color: #008288"></i>
+            <h2 class="alex-brush-regular mt-4" style="color: #008288; font-size: 38px;">Cím:</h2>
+            <p>7030 Paks, Vasút utca 10.</p>
+          </div>
+        </div>
       </div>
     </div>
+
+    
   
   </div> --}}
+
+  <div class="container d-flex justify-content-center">
+    <div class="col-12 col-lg-10">
+      <div class="row row-cols-1 row-cols-md-3"> <!-- Добавлен класс `g-4` для отступов между колонками -->
+        <div class="col  ">
+          <div class="text-center card mb-5 rounded-3 shadow-sm pt-4">
+            <div class="card-body">
+              <i class="fa-solid fa-envelope fa-2xl" style="color: #008288"></i>
+              <h2 class="alex-brush-regular mt-4" style="color: #008288; font-size: 38px;">Email:</h2>
+              <p>info@humanregen.hu</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col  ">
+        <div class="text-center card mb-5 rounded-3 shadow-sm pt-4">
+          <div class="card-body">
+            <i class="fa-solid fa-phone fa-2xl" style="color: #008288"></i>
+            <h2 class="alex-brush-regular mt-4" style="color: #008288; font-size: 38px;">Telefon:</h2>
+            <p>+36 30 39 677 67</p>
+          </div>
+        </div>
+        </div>
+        <div class="col  ">
+        <div class="text-center card mb-5 rounded-3 shadow-sm pt-4">
+          <div class="card-body">
+            <i class="fa-solid fa-location-dot fa-2xl" style="color: #008288"></i>
+            <h2 class="alex-brush-regular mt-4" style="color: #008288; font-size: 38px;">Cím:</h2>
+            <p>7030 Paks, Vasút utca 10.</p>
+          </div>
+        </div>
+      </div>
+
+      </div>
+    </div>
+  </div>
+  
 
   <div class="px-4 pt-5 my-5 text-center ">
     <h2 class="alex-brush-regular" style="color: #008288; font-size: 38px;">Térkép</h2>
 
-    <div class="col-lg-10 mx-auto">
+    <div class="mx-auto">
 
       <p class="lead mb-4">Az alábbi címen találhat meg minket</p>
       <!-- <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p> -->
@@ -106,7 +160,7 @@
     </div>
 
     <div class="container d-flex justify-content-center align-items-center">
-      <div class="col-11">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10 col-xxl-10">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2740.9546746208853!2d18.8517212!3d46.60789869999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47425c6b0503bd07%3A0x4426f920bc68de44!2sPaks%2C%20Vas%C3%BAt%20u.%2010%2C%207030%20Magyarorsz%C3%A1g!5e0!3m2!1shu!2sua!4v1731274488035!5m2!1shu!2sua" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
@@ -119,9 +173,9 @@
     <h2 class="alex-brush-regular" style="color: #008288; font-size: 38px;">Töltsd fel a tested és ébredj új életre</h2>
 
     <h1 class="display-4 fw-bold text-body-emphasis">Humán Regenerátor Sports</h1>
-    <div class="col-lg-10 col-xl-10 col-xxl-8 mx-auto">
+    <div class="col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10 col-xxl-8 mx-auto">
 
-      <p class="lead mb-4">A Humán Regenerátor Sports kezelés <strong>orvosi alapja egy 18 éves kutatássorozat eredményeként </strong>megszületett és folyamatosan fejlesztett&nbsp;világszerte egyedülálló high-tech eszköz, amely <strong>sejtszinten aktiválja a szervezet öngyógyító folyamatait, ellensúlyozza a sejtekben lévő oxidatív stresszt és akkumulátorként tölti fel a sejteket</strong>. A<strong> Humán Regenerátor Sports a piacon elérhető legerősebb generátorral büszkélkedhet</strong>. Kimagasló generátor teljesítményével túlszárnyalja a Humán Regenerátor Power Jet és Professionel modelleket, kifejezetten erős hatást gyakorolva már a vázizmokra is.</p>
+      <p class="lead mb-4 ">A Humán Regenerátor Sports kezelés <strong>orvosi alapja egy 18 éves kutatássorozat eredményeként </strong>megszületett és folyamatosan fejlesztett&nbsp;világszerte egyedülálló high-tech eszköz, amely <strong>sejtszinten aktiválja a szervezet öngyógyító folyamatait, ellensúlyozza a sejtekben lévő oxidatív stresszt és akkumulátorként tölti fel a sejteket</strong>. A<strong> Humán Regenerátor Sports a piacon elérhető legerősebb generátorral büszkélkedhet</strong>. Kimagasló generátor teljesítményével túlszárnyalja a Humán Regenerátor Power Jet és Professionel modelleket, kifejezetten erős hatást gyakorolva már a vázizmokra is.</p>
       <!-- <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p> -->
 
     </div>
